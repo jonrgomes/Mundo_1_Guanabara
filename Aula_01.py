20a3b0e274c0d3f04d5b699d5c7bb39cabc437e5
jonrgomes <jonrgomes@gmail.com>
2026-07-30 19:59:23 -0300

    Mundo_1

--- Aula_01.py
+++ Aula_01.py
@@ -1,1 +1,33 @@
+# (print) recebe
 print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+print('Olá', 5)
+
+# Variáveis 
+# (=) recebe
+nome = 'Jonatas'
+idade = 33
+peso = 72.5
+print(nome,idade,peso)
+
+# (input) leia
+nome = input('Qual o seu nome: ')
+idade = input('Qual a sua idade: ')
+peso = input('Qual o seu peso: ')
+
+# Desafio_01
+nome = input('Qual o seu nome? ')
+print(f'Prazer em conhece-lo, {nome}!')
+
+# Desafio_02
+dia = input('Que dia você nasceu: ')
+mes = input('Qual mês você nasceu: ')
+ano = input('Qual ano você nasceu: ')
+print(f'Você nasceu em {dia} de {mes} de {ano}!')
+
+# Desafio_03
+n_1 = int(input('Digite um número: '))
+n_2 = int(input('Digite outro número: '))
+s = n_1 + n_2
+print(f'A soma entre {n_1} e {n_2} é {s}!')        
